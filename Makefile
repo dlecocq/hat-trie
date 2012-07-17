49ad1fcd997ede9d8dd418cac037dc567d137106
Dan Lecocq <dan@seomoz.org>
2012-07-16 18:51:27 -0700

    std::vector implementation

--- Makefile
+++ Makefile
@@ -1,15 +1,30 @@
 CPP	 = g++
-CPPOPTS = -O9 -Wall -Iinclude -I/Users/dan/playground/hat-trie-chris/hat-trie/src
+CPPOPTS = -O3 -Wall -Iinclude -I/Users/dan/playground/hat-trie-chris/hat-trie/src
 
 all: test bench
 
-bench: include/*.hpp bench.cpp
-	$(CPP) $(CPPOPTS) bench.cpp -o bench
+# Retrieve some of the test data from Dr. Askitis
+data/distinct_1:
+	# Create a directory
+	mkdir -p data
+	# Downloading the dataset
+	curl http://www.naskitis.com/distinct_1.bz2 -o data/distinct_1.bz2
+	# Unpacking the data
+	bunzip2 data/distinct_1.bz2
+	# Splitting the data up into the various inputs
+	for i in {1,2,4,6,8,{1,2,3,4,5,6,7,8,9}{0,5}}00 ; do
+		head -n $i data/distinct_1 > data/distinct_$i
+	done
+	for i in {1,2,3,4,5,6,7,8,9}{0,5}000 ; do
+		head -n $i data/distinct_1 > data/distinct_$i
+	done
 
-clean:
-	rm -rdf *.o test bench *.dSYM
+bench: include/*.hpp bench.cpp data/distinct_1
+	$(CPP) $(CPPOPTS) bench.cpp -o bench
+	./bench ./data/distinct_{1,2,4,6,8,{1,2,3,4,5,6,7,8,9}{0,5}}00 ./data/distinct_{1,2,3,4,5,6,7,8,9}{0,5}000
 
 test: include/*.hpp test.cpp
 	$(CPP) $(CPPOPTS) -Ideps/Catch/single_include test.cpp -o test
-	# Now invoke
-	#./test
+
+clean:
+	rm -rdf *.o test bench *.dSYM
--- test.cpp
+++ test.cpp
@@ -2,15 +2,7 @@
 #include "catch.hpp"
 
 #include <hash.hpp>
-#include <hat-set.hpp>
-#include <hat-map.hpp>
 #include <array-hash.hpp>
-
-struct cmp_int {
-    const char* operator()(const int& i) {
-        return reinterpret_cast<const char*>(&i);
-    }
-};
 
 TEST_CASE("hashes", "Implements hashes") {
     /* Our hash object */
@@ -42,126 +34,6 @@ TEST_CASE("array-hash", "Implements the Array Hash") {
             /* Update */
             hash["testing"] = 10;
             REQUIRE(hash["testing"] == 10);
-        }
-        
-        SECTION("with length", "Can insert with length provided") {
-            REQUIRE(hash.exists("foob") == false);
-            /* Insert 5 with the word 'test' */
-            hash.insert("foobar", 4, 5);
-            REQUIRE(hash.exists("foob"));
-            /* There seems to be some fishiness going on here, where if I 
-             * include `hash["foob"]` in the REQUIRE block, it does not return
-             * what it should. However, storing in an integer is fine */
-            REQUIRE(hash["foob"] == 5);
-        }
-        
-        SECTION("binary key", "Can insert with a binary key") {
-            int   i = 5;
-            char* p = reinterpret_cast<char*>(&i);
-            REQUIRE(hash.exists(p, sizeof(int)) == false);
-            hash.insert(p, sizeof(int), 10);
-            REQUIRE(hash.exists(p, sizeof(int)));
-            REQUIRE(hash.get(p, sizeof(int)) == 10);
-        }
-    }
-    
-    SECTION("remove", "Implements removal") {
-        SECTION("char*", "Can remove with char*") {
-            REQUIRE(hash.exists("chardel") == false);
-            hash["chardel"] = 10;
-            REQUIRE(hash.exists("chardel"));
-            REQUIRE(hash["chardel"] == 10);
-            hash.remove("chardel");
-            REQUIRE(hash.exists("chardel") == false);
-        }
-        
-        SECTION("binary", "Can remove with binary data") {
-            int   i = 10;
-            char *p = reinterpret_cast<char*> (&i);
-            REQUIRE(hash.exists(p, sizeof(int)) == false);
-            hash.insert(p, sizeof(int), 10);
-            REQUIRE(hash.exists(p, sizeof(int)));
-            hash.remove(p, sizeof(int));
-            REQUIRE(hash.exists(p, sizeof(int)) == false);
-        }
-        
-        SECTION("string", "Can remove using a string") {
-            std::string s("string");
-            REQUIRE(hash.exists(s) == false);
-            hash[s] = 10;
-            REQUIRE(hash.exists(s));
-            hash.remove(s);
-            REQUIRE(hash.exists(s) == false);
-        }
-    }
-}
-
-TEST_CASE("hat-set", "The HAT set implementation") {
-    SECTION("constructors", "Implements all the correct constructors") {
-        SECTION("default", "Default constructor") {
-            ht::Set<int> int_s;
-            ht::Set<std::string> string_s;
-            REQUIRE(   int_s.size() == 0);
-            REQUIRE(string_s.size() == 0);
-        }
-        
-        SECTION("special comparator", "Can provide a special comparator") {
-            /* Using a comparator, default constructor */
-            ht::Set<int, cmp_int> s;
-            REQUIRE(s.size() == 0);
-        }
-        
-        SECTION("iterators", "Can be initialized with iterators") {
-            std::vector<int> values;
-            for (int i = 0; i < 10; ++i) {
-                values.push_back(i);
-            }
-            
-            ht::Set<int> s(values.begin(), values.end());
-            REQUIRE(s.size() == 10);
-        }
-        
-        SECTION("copy constructor", "We can copy a set") {
-            ht::Set<int> s1;
-            for (int i = 0; i < 10; ++i) {
-                s1.insert(i);
-            }
-            ht::Set<int> s2(s1);
-            REQUIRE(s2.size() == 10);
-        }
-    }
-    
-    SECTION("iterators", "Can traverse it") {
-        ht::Set<std::string> s;
-        
-        /* Let's insert a bunch of words */
-        std::cout << "Inserting hello" << std::endl;
-        s.insert("hello");
-        std::cout << "Inserting how" << std::endl;
-        s.insert("how");
-        s.insert("are");
-        s.insert("you");
-        s.insert("today");
-        
-        SECTION("begin/end", "Can traverse in forward order") {
-            ht::Set<std::string>::iterator it(s.begin());
-            std::vector<std::string> found;
-            for(; it != s.end(); ++it) {
-                found.push_back(*it);
-                std::cout << "Just got " << *it << std::endl;
-            }
-            REQUIRE(found[0] == "are");
-            REQUIRE(found[4] == "today");
-        }
-        
-        SECTION("rbegin/rend", "Can traverse in reverse order") {
-            ht::Set<std::string>::reverse_iterator it(s.rbegin());
-            std::vector<std::string> found;
-            for(; it != s.rend(); ++it) {
-                found.push_back(*it);
-            }
-            REQUIRE(found[0] == "today");
-            REQUIRE(found[4] == "are");
         }
     }
 }
